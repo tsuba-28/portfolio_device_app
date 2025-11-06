118564ffc7a6bd419ec764c4315a69ed0c3b4ba9
Tsuba <>
2025-11-06 23:33:32 +0900

    usernameカラムを含んだテーブルに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_03_163245) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_06_141320) do
   create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -21,5 +22,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_03_163245) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
