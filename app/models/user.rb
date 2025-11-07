4c435790841b2bb48a6fe5b833af3dd8dc4ab951
Tsuba <>
2025-11-08 01:38:29 +0900

    username に必須、文字数制限、正規表現の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 3, maximum: 20 }
+  validates :username, format: { with: /\A[a-zA-Z0-9_-]+\z/ }
 end
