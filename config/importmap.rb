67feef87047c2eb2c17e3f87703d79721fd9deb0
Tsuba <>
2025-11-15 01:24:52 +0900

    bootstrap 削除 & importmap の preload など環境設定の更新。

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.js"
+pin "@hotwired/stimulus", to: "stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
-pin "bootstrap" # @5.3.8
