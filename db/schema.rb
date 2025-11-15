aeb3c93e5ec4846b70bdcd7bcc74fd1c7db1095a
Tsuba <>
2025-11-15 23:47:27 +0900

    Profileモデルの追加とUserとの1対1関連付けを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_06_141320) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_15_141923) do
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "nickname"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
@@ -24,4 +33,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_06_141320) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "profiles", "users"
 end
