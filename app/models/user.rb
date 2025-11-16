2d5ad627ac74c65cfb02f0d9efb8a5c0a7d36a8a
Tsuba <>
2025-11-16 23:16:18 +0900

    ユーザー作成時に自動で空（デフォルト）のプロフィールを生成する処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :profile, dependent: :destroy
+
+  after_create :create_default_profile
+
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, length: { minimum: 3, maximum: 20 }
   validates :username, format: { with: /\A[a-zA-Z0-9_-]+\z/ }
+
+  private
+    def create_default_profile
+      create_profile
+    end
 end
