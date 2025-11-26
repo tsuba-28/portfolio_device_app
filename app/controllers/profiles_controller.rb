13f0212b14ad8f88d40bd774b033c8f291c689a6
Tsuba <>
2025-11-26 23:00:51 +0900

    プロフィール編集機能にアバター画像アップロードを追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,33 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_profile
 
   def show
-    @profile ||= current_user.create_profile
   end
 
   def edit
-    @profile ||= current_user.create_profile
   end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profile_path, notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_profile
+      @profile = current_user.profile || current_user.create_profile
+    end
+
+    def profile_params
+      params.require(:profile).permit(
+        :nickname,
+        :bio,
+        :avatar
+      )
+    end
 
 end
