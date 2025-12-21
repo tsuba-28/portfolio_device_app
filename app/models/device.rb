fbb97988943f8c46989390e8bebe78c54841c114
Tsuba <>
2025-12-21 23:17:19 +0900

    Categoryモデルにvalidatesの追加、seedにマスターデータの作成。

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,5 @@
-class Category < ApplicationRecord
-  has_many :devices, dependent: :restrict_with_error
+class Device < ApplicationRecord
+  belongs_to :category
 
   validates :name, presence: true
   validates :slug, presence: true, uniqueness: true
