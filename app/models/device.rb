9ec1a9bd4e605ebc6635bfb52bafdbf114ea17ce
Tsuba <>
2026-01-04 23:01:58 +0900

    多対多が機能できるように変更 記述不足によるエラーを起きないように

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,9 @@
 class Device < ApplicationRecord
   belongs_to :category
 
+  has_many :desk_setup_devices, dependent: :destroy
+  has_many :desk_setups, through: :desk_setup_devices
+
   validates :name, presence: true
   validates :slug, presence: true, uniqueness: true
 end
