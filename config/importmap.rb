6ab576faa863f425a298ac6a0cb0fe2f8089c51e
Tsuba <>
2026-02-05 23:53:19 +0900

    TomSelectを利用した自由入力（Zenn風）のタグ機能。

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.js"
 pin "@hotwired/stimulus", to: "stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.4.3/dist/js/tom-select.complete.min.js"
+pin "@orchidjs/sifter", to: "@orchidjs--sifter.js" # @1.1.0
+pin "@orchidjs/unicode-variants", to: "@orchidjs--unicode-variants.js" # @1.1.2
