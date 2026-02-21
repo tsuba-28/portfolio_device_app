9b997ff2e3c9fc63b7028807cad796c18a972929
Tsuba <>
2026-02-22 04:41:56 +0900

    profileにusertagを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_21_144154) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_21_190206) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -87,6 +87,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_21_144154) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "usertag", default: "", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
