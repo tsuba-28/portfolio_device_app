c242b88c12fc3f1590d7fb634dc7355d0750d659
Tsuba <>
2026-02-21 23:51:08 +0900

    ユーザータグに関する制約の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_16_142701) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_21_144154) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -139,11 +139,12 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_16_142701) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "usertag"
+    t.string "usertag", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username", "usertag"], name: "index_users_on_username_and_usertag", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["usertag"], name: "index_users_on_usertag"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
