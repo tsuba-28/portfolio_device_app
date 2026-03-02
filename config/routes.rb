7306c6dd1b2c6900662a40877ff9dd115bd4adf9
Tsuba <>
2026-03-02 22:35:45 +0900

    indexの修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resource :profile, only: [:show, :edit, :update]
 
-  resources :desk_setups, only: [:show, :new, :create] do
+  resources :desk_setups, only: [:index, :show, :new, :create] do
     collection do
       post :add_device
     end
