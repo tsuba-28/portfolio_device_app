007aa5a806b7b1e61ebd4c978011d57c26597c5a
Tsuba <>
2026-03-11 05:32:37 +0900

    整理の為不要なファイルの削除

--- db/migrate/20251120141012_add_usertag_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddUsertagToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :usertag, :string
-
-    add_index :users, [:username, :usertag], unique: true
-  end
-end
