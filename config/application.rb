afe3953b868f44935fd8202c3c02e2df42486e91
Tsuba <>
2026-03-12 01:25:07 +0900

    表示される時間を東京時間に

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module PortfolioDeviceApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     config.exceptions_app = self.routes
   end
