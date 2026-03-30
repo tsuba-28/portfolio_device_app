cdad7ce4f5cdf1225d915dac32261fc1e6ce12ba
Tsuba <>
2026-03-30 21:38:01 +0900

    `arel_table`を使用して大文字、小文字の区別をなくして検索できるように。 デバイス関連とカテゴリー関連にて部分一致、完全一致（大文字小文字区別無し）にて分けてヒットするように設定

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,11 +1,22 @@
 class DevicesController < ApplicationController
   def search
     if params[:q].present?
-      @devices = Device.where("brand_name LIKE :keyword OR name LIKE :keyword", keyword:  "%#{params[:q]}%")
+      partial_keyword = "%#{params[:q]}%"  #部分一致
+      exact_keyword   = params[:q]         #完全一致
+
+      devices_table = Device.arel_table
+      categories_table = Category.arel_table
+
+      #カラムごとに一致条件を変更
+      @devices = Device.joins(:category).where(
+        devices_table[:brand_name].matches(partial_keyword)
+        .or(devices_table[:name].matches(partial_keyword))
+        .or(categories_table[:name].matches(exact_keyword))
+        .or(categories_table[:name_ja].matches(exact_keyword))
+      )
     else
       @devices = Device.order(created_at: :desc).limit(10)
     end
-    # render layout: false if turbo_frame_request?
   end
 
   private
